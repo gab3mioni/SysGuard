9e0f23c11875004bf0ce38df20396da4b2931a54
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-11-20 20:21:06 -0300

    refactor: Removendo endpoint para listar os containers docker

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_cors import CORS
 from .utils import get_system_metrics
-from .docker_utils import list_containers
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 CORS(bp, resources={r"/api/*": {"origins": "http://localhost:3001"}})
@@ -16,9 +15,3 @@ def metrics():
     """Retorna métricas do sistema"""
     metrics = get_system_metrics()
     return jsonify(metrics)
-
-@bp.route('/docker/containers', methods=['GET'])
-def docker_containers():
-    """Lista os containers Docker em execução"""
-    containers = list_containers()
-    return jsonify(containers)
