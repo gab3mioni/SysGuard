c373600131cc6d55e3492cb7364a55712cae8db1
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-11-20 20:22:31 -0300

    docs: Adequando docstrings ao routes.py

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -7,11 +7,34 @@ CORS(bp, resources={r"/api/*": {"origins": "http://localhost:3001"}})
 
 @bp.route('/status', methods=['GET'])
 def status():
-    """Endpoint para verificar o status do backend."""
+    """
+    Endpoint para verificar o status do backend.
+
+    Retorna:
+        json: Um objeto JSON com a mensagem indicando que o backend está online.
+        Exemplo de resposta:
+        {
+            "status": "Back online!"
+        }
+    """
     return jsonify({"status": "Back online!"})
 
 @bp.route('/metrics', methods=['GET'])
 def metrics():
-    """Retorna métricas do sistema"""
+    """
+    Endpoint para obter as métricas do sistema
+
+    Utiliza a função get_system_metrics() para coletar informações de desempenho
+    e recursos do sistema, como uso de CPU, memória e disco.
+
+    Retorna:
+        json: Um objeto JSON contendo as métricas do sistema.
+        Exemplo de resposta:
+        {
+            "cpu_usage": 0.0,
+            "memory_usage": 1.6,
+            "disk_usage": 17.2,
+        }
+    """
     metrics = get_system_metrics()
     return jsonify(metrics)
