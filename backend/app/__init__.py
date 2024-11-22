d8b09c3692d3a1a7aa530ed1554e1c624fc8ff83
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-11-22 15:47:00 -0300

    fix: Correção na configuração e CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes import register_routes
 from .database import db
+from .config.settings import Config
 
 def create_app():
+    config = Config()
     app = Flask(__name__)
-    app.config.from_object('config.settings.Config')
+
+    CORS(app, resources={r"/api/*": {"origins": "http://localhost:3001"}})
+    app.config.from_object(config)
 
     db.init_app(app)
 
--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -1,4 +1,5 @@
 class Config:
-    JSONIFY_PRETTYPRINT_REGULAR = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    def __init__(self):
+        self.JSONIFY_PRETTYPRINT_REGULAR = True
+        self.SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
+        self.SQLALCHEMY_TRACK_MODIFICATIONS = False
